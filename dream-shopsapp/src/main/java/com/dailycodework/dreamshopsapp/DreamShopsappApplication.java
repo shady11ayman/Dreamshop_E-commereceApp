45eb284a7ee59a0c5ea0b05c15628ccbf00bfd78
Shady Ayman <63479743+shady11ayman@users.noreply.github.com>
2026-03-28 17:45:53 +0200

    Add files via upload

---
+++ dream-shopsapp/src/main/java/com/dailycodework/dreamshopsapp/DreamShopsappApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.dreamshopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamShopsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamShopsappApplication.class, args);
+	}
+
+}
---
+++ dream-shopsapp/src/test/java/com/dailycodework/dreamshopsapp/DreamShopsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.dreamshopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
